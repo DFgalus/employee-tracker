246faa16bf10af560d8f5e79845b736ae6de2446
Dgalus <damiangalus7@gmail.com>
2023-02-27 12:01:17 -0500

    Added function to add new department

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ function addEmployee() {
   })
  }
 
- //Update employee role
+//Update employee role
  function updateEmployeeRole() {
     connection.query("SELECT employees.lastName, role.title FROM employees JOIN role ON employees.roleID = role.id;", 
     (err, res) => {
@@ -297,3 +297,34 @@ function addEmployee() {
             });
         });
   }
+
+//Add department
+function addDept() { 
+
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What Department would you like to add? "
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is the new Department ID number? "
+          }
+
+    ]).then(function(answers) {
+        connection.query("INSERT INTO department SET ? ",
+            {
+              name: answers.name,
+              id: answers.id
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                runEmployeeDB();
+            }
+        )
+    })
+  }
+
